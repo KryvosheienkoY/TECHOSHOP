0bd89d762968b8964fd2868e8dbe64d5c12336e2
KryvosheienkoY <j.kryvosheienko@gmail.com>
2018-11-29 15:52:37 +0200

    Table of products in cart is added

--- src/modules/CartContentHTML.js
+++ src/modules/CartContentHTML.js
@@ -14,21 +14,48 @@ function Product(id, quantity) {
 function generateCartContent() {
     let contentDiv = $('#contentDIVID');
     contentDiv.empty();
-
     let cart = JSON.parse(localStorage.getItem('cart'));
-    let length =cart.products.length;
-    if(length<1){
+    let length = cart.products.length;
+    if (length < 1) {
         let items = $(`<p class="cartInfo">`).text("Your cart is empty :( ");
         contentDiv.append(items);
     }
     else {
-        let productCart=$('<div class="productCartGrid">');
-        for (let i = 0; i < length; i++) {
-            productCart.append(_makeHtmlCartProduct(cart.products[i]));
-        }
+        let productCart = $(`<div class="table-responsive">`);
+        productCart.append($(`<table class="table" id="productCartGridID">`));
         contentDiv.append(productCart);
+        let totalPrice = 0;
+        for (let i = 0; i < length; i++) {
+            let product = cart.products[i];
+            let price=0;
+            if(pro)
+            totalPrice= totalPrice+ pro
+            loadProduct(product.id, product.quantity, productCart);
+        }
+        $('#productCartGridID').append($(`<tfoot><tr class="totalRow"><td colspan="3">Subtotal</td>' +
+        '<td id="totalCell"></td></tfoot>`));
     }
 }
+
+
+function loadProduct(currentProductID, quantity) {
+    let urlString = String("https://nit.tron.net.ua/api/product/" + currentProductID);
+    console.log(urlString);
+    jQuery.ajax({
+        url: urlString,
+        method: 'get',
+        dataType: 'json',
+        success: function (json) {
+            console.table(json);
+            console.log("Loaded product " + currentProductID + " via AJAX!");
+           $('#productCartGridID').append(_makeHtmlCartProduct(json, quantity));
+        },
+        error: function (xhr) {
+            alert("An error occured: " + xhr.status + " " + xhr.statusText);
+        },
+    });
+}
+
 
 function addToCart(productID) {
     if (localStorage && localStorage.getItem('cart')) {
@@ -64,23 +91,30 @@ let _makeHtmlCartProduct = ({
                                 description,
                                 price,
                                 special_price,
-                            }) => {
-    let $product = $(`<div class="productInfoPage">`);
-    $product.append($(`<img src="${image_url}" alt="${name}" class="img-fluid product-Bigimage">`));
-    $product.append($(`<p class="product-title">`).text(name));
+                            }, {quantity}) => {
+    let $product = $(`<tr class="productCartInfo">`);
+    $product.append($(`<td class="imWrap"><img src="${image_url}" alt="${name}" class="img-fluid product-image"></td>`));
+    $product.append($(`<td class="product-title">`).text(name));
+    let priceTd = $(`<td class="product-price">`);
     if (special_price == null) {
-        $product.append($(`<span class="product-price">`).text(price));
+        priceTd.append($(`<p class="product-price">`).text(price));
     }
     else {
-        $product.append($(`<p class="product-price old_price">`).text(price));
-        $product.append($(`<p class="product-price special_price">`).text(special_price));
+        priceTd.append($(`<p class="product-price old_price">`).text(price));
+        priceTd.append($(`<p class="product-price special_price">`).text(special_price));
     }
-    let buyButton = $(`<button type="button" class="product-buy-button btn btn-success"><i class="fas fa-shopping-cart"></i> Buy</button>`);
-    buyButton.on("click", addToCart.bind(null, id));
-    $product.append(buyButton);
-    $product.append($(`<p class="product-description">`).text(description));
+    $product.append(priceTd);
+
+// <div class="sd-cart-qty" style="white-space: nowrap;">
+//         <input type="button" value="-" style="visibility: visible;">
+//         <input min="0" class="sd-line-item-qty" size="5" type="text" value="9" name="order[line_items_attributes][0][quantity]" id="order_line_items_attributes_0_quantity" style="width: 24px;" maxlength="5">
+//         <input type="button" value="+">
+//         </div>
+    console.log("quantity - " + quantity);
+    $product.append($(`<td class="product-quantity">`).text(quantity));
     return $product;
 };
+
 
 export {
     generateCartContent,
